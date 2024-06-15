b96e86140425833eb561c56383ff8a6dc0cbe6ac
Rida Sukmara <ridasukmara@hotmail.com>
2024-06-15 07:46:35 +0700

    initial commit

---
+++ Movie App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie App
+//
+//  Created by Rida Sukmara on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
